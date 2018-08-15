607f7f5bb88122a58cad3bc37c0d7886aab99b7e
Alina Zaitseva <Alina_Zaitseva@epam.com>
2018-08-15 18:51:43 +0300

    added text field validator

--- CardSample/CardSample/ViewControllers/ViewController.swift
+++ CardSample/CardSample/ViewControllers/ViewController.swift
@@ -8,19 +8,75 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate  {
 
-    @IBOutlet weak var nameOnCard: UITextField!
-    @IBOutlet var cardNumber: [UITextField]!
-    @IBOutlet weak var expireDate: UITextField!
-    @IBOutlet weak var securityCode: UITextField!
+    @IBOutlet weak var nameOnCardTextField: UITextField!
+    @IBOutlet weak var expireDateTextField: UITextField!
+    @IBOutlet weak var securityCodeTextField: UITextField!
+    var creditCard: CardEntity?
+    var name: String?
+    var cardNumber: [String]?
+    var expireDate: String?
+    var cvv: String?
+    
+    @IBOutlet weak var firstPartCardNumberTextField: UITextField!
+    @IBOutlet weak var secondPartCardNumberTextField: UITextField!
+    @IBOutlet weak var thirdPartCardNumberTextField: UITextField!
+    @IBOutlet weak var fourthPartCardNumberTextField: UITextField!
+    
     
     @IBAction func pushedAddCard(_ sender: UIButton) {
+        creditCard = CardEntity(name: name,
+                                cardNumber: "111",
+                                expireDate: expireDate!,
+                                cvv: cvv!)
+        print(creditCard)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        secondPartCardNumberTextField.isEnabled = false
+        thirdPartCardNumberTextField.isEnabled = false
+        fourthPartCardNumberTextField.isEnabled = false
+        
+    }
+    @IBAction func editingCardNumber(_ sender: UITextField) {
+        if (sender.text?.count)! == 4 {
+            cardNumber?.append(sender.text!)
+            switch sender {
+            case firstPartCardNumberTextField :
+                secondPartCardNumberTextField.isEnabled = true
+            case secondPartCardNumberTextField :
+                thirdPartCardNumberTextField.isEnabled = true
+            case thirdPartCardNumberTextField :
+                fourthPartCardNumberTextField.isEnabled = true
+            default:
+                print(sender)
+            }
+        }
     }
     
+    
+    @IBAction func editingTextField(_ sender: UITextField) {
+        switch sender {
+        case nameOnCardTextField:
+            name = sender.text
+        case expireDateTextField:
+           expireDate = sender.text
+        case securityCodeTextField:
+            cvv = sender.text
+        default:
+            print(sender.text)
+        }
+    }
 }
 
+
+struct CardEntity {
+    
+    var name: String?
+    var cardNumber: String
+    var expireDate: String
+    var cvv: String
+
+}

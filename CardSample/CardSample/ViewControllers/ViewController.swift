61d33a6ba672561820f6b18f11babb0abb6ce20e
Alina Zaitseva <Alina_Zaitseva@epam.com>
2018-08-15 15:17:48 +0300

    added text field outlets

--- CardSample/CardSample/ViewControllers/ViewController.swift
+++ CardSample/CardSample/ViewControllers/ViewController.swift
@@ -10,16 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameOnCard: UITextField!
+    @IBOutlet var cardNumber: [UITextField]!
+    @IBOutlet weak var expireDate: UITextField!
+    @IBOutlet weak var securityCode: UITextField!
+    
+    @IBAction func pushedAddCard(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
